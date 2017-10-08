0c8dad59cf6eb83ba65db0c0b3e785a55f7f022b
Max Barraclough <maxbarraclough@users.noreply.github.com>
2017-10-08 19:53:32 +0100

    testIsValidSwizzleVector

--- src/txtshuffle/VectorConversionsTests.java
+++ src/txtshuffle/VectorConversionsTests.java
@@ -51,13 +51,30 @@ public final class VectorConversionsTests {
 
 	// TODO IMPLEMENT
 
-//	/**
-//	 * Test method for {@link txtshuffle.VectorConversions#isValidSwizzleVector(int[])}.
-//	 */
-//	@Test
-//	public final void testIsValidSwizzleVector() {
-//		fail("Not yet implemented");
-//	}
+	/**
+	 * Test method for {@link txtshuffle.VectorConversions#isValidSwizzleVector(int[])}.
+	 */
+	@Test
+	public final void testIsValidSwizzleVector()
+	{
+		final int[] valid1 = new int[] {0,1,2,3,4};
+		final int[] valid2 = new int[] {5,2,4,1,0,3};
+		final int[] valid3 = new int[] {};
+		final int[] valid4 = new int[] {0};
+
+		final int[] invalid1 = new int[] {1};
+		final int[] invalid2 = new int[] {0,1,3};
+		final int[] invalid3 = new int[] {0,1,-1};
+
+		org.junit.Assert.assertTrue(VectorConversions.isValidSwizzleVector(valid1));
+		org.junit.Assert.assertTrue(VectorConversions.isValidSwizzleVector(valid2));
+		org.junit.Assert.assertTrue(VectorConversions.isValidSwizzleVector(valid3));
+		org.junit.Assert.assertTrue(VectorConversions.isValidSwizzleVector(valid4));
+
+		org.junit.Assert.assertFalse(VectorConversions.isValidSwizzleVector(invalid1));
+		org.junit.Assert.assertFalse(VectorConversions.isValidSwizzleVector(invalid2));
+		org.junit.Assert.assertFalse(VectorConversions.isValidSwizzleVector(invalid3));
+	}
 //
 //	/**
 //	 * Test method for {@link txtshuffle.VectorConversions#isValidCompactVector(int[])}.
