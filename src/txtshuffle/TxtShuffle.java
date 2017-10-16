687c7c28b7f3a86e2adc17d1cfd9200c7f167834
Max Barraclough <maxbarraclough@users.noreply.github.com>
2017-10-16 21:40:02 +0100

    Yet more renaming corrections

--- src/txtshuffle/TxtShuffle.java
+++ src/txtshuffle/TxtShuffle.java
@@ -111,11 +111,11 @@ public final class TxtShuffle {
 	public static BigInteger retrieveNumberFromData(final String[] data)
 	{
 //		final int[] retrievedSortingACTUALIsv = TxtShuffle.findSortingACTUALIsv(data);
-//		final int[] retrievedUseful = TxtShuffle.invertSwizzleVector(retrievedSortingACTUALIsv);
+//		final int[] retrievedUseful = TxtShuffle.invertACTUALIsv(retrievedSortingACTUALIsv);
 
-		final int[] retrievedUseful = TxtShuffle.findInverseOfSortingSwizzleVec(data);
+		final int[] retrievedUseful = TxtShuffle.findInverseOfSortingACTUALIsv(data);
 
-		final int[] retrievedCompact = VectorConversions.swizzleToCompact(retrievedUseful);
+		final int[] retrievedCompact = VectorConversions.ACTUALisvToCompact(retrievedUseful);
 
 		final BigInteger retrievedNum = VectorConversions.compactVectorToInt(retrievedCompact);
 
@@ -151,7 +151,7 @@ public final class TxtShuffle {
 
 	}
 
-	// All this swizzle vec business is analogous to matrix product, but that wouldn't buy us anything in implementation
+	// All this ISV business is analogous to matrix product, but that wouldn't buy us anything in implementation
 
 
 
@@ -188,14 +188,14 @@ public final class TxtShuffle {
 
 
 
-	// TODO the final inversion stage in findSwizzleVector can be omitted if we
-	// just apply the inverse swizzle vector, instead.
+	// TODO the final inversion stage in findACTUALIsv can be omitted if we
+	// just apply the ACTUALsv, instead.
 	// This shouldn't be any harder.
 
 
 /**
  * AVOID this method - we want to avoid doing that invert!
- * Find the swizzle vector which transforms the original data into its sorted order
+ * Find the ACTUALISV which transforms the original data into its sorted order
  * @param inputData
  * @return
  */
@@ -203,28 +203,28 @@ public final class TxtShuffle {
 	{
 		// We sort, but we sort an int array, treating them as indices into our data.
 		// The result is an int array which specifies the order of the sorted data.
-		// This *isn't* the same thing as the swizzle-vec which transforms the data into its sorted order!
-		// It's actually the *inverse*. So we 'invert' the swizzle vector, and return that.
+		// This *isn't* the same thing as the ACTUALISV which transforms the data into its sorted order!
+		// It's actually the *inverse*. So we 'invert' the ACTUALISV, and return that.
 
-		final Integer[] swizzleVecBoxed = new Integer[inputData.length];
+		final Integer[] ACTUALisvBoxed = new Integer[inputData.length];
 
-		for (int i = 0; i != swizzleVecBoxed.length; ++i)
+		for (int i = 0; i != ACTUALisvBoxed.length; ++i)
 		{
-			swizzleVecBoxed[i] = i;
+			ACTUALisvBoxed[i] = i;
 		}
 
 		final CustomIntegerComparator c = new CustomIntegerComparator(inputData);
 
-		java.util.Arrays.sort(swizzleVecBoxed,c);
+		java.util.Arrays.sort(ACTUALisvBoxed,c);
 
 
 		// Laboriously unbox
 
-		final int[] unboxedArr = new int[swizzleVecBoxed.length];
+		final int[] unboxedArr = new int[ACTUALisvBoxed.length];
 
 		for (int i = 0; i != unboxedArr.length; ++i)
 		{
-			unboxedArr[i] = swizzleVecBoxed[i];
+			unboxedArr[i] = ACTUALisvBoxed[i];
 		}
 
 		// Invert
@@ -240,36 +240,36 @@ public final class TxtShuffle {
 
 /**
  * AVOID this method - we want to avoid doing that invert!
- * Find the swizzle vector which transforms the original data into its sorted order
+ * Find the ACTUALISV which transforms the original data into its sorted order
  * @param inputData
  * @return
  */
-	public static int[] findInverseOfSortingSwizzleVec(final String[] inputData)
+	public static int[] findInverseOfSortingACTUALIsv(final String[] inputData)
 	{
 		// We sort, but we sort an int array, treating them as indices into our data.
 		// The result is an int array which specifies the order of the sorted data.
-		// This *isn't* the same thing as the swizzle-vec which transforms the data into its sorted order!
+		// This *isn't* the same thing as the ACTUALISV which transforms the data into its sorted order!
 		// It's actually the *inverse*. Fortunately, this is just as useful.
 
-		final Integer[] swizzleVecBoxed = new Integer[inputData.length];
+		final Integer[] ACTUALisvBoxed = new Integer[inputData.length];
 
-		for (int i = 0; i != swizzleVecBoxed.length; ++i)
+		for (int i = 0; i != ACTUALisvBoxed.length; ++i)
 		{
-			swizzleVecBoxed[i] = i;
+			ACTUALisvBoxed[i] = i;
 		}
 
 		final CustomIntegerComparator c = new CustomIntegerComparator(inputData);
 
-		java.util.Arrays.sort(swizzleVecBoxed,c);
+		java.util.Arrays.sort(ACTUALisvBoxed,c);
 
 
 		// Laboriously unbox
 
-		final int[] unboxedArr = new int[swizzleVecBoxed.length];
+		final int[] unboxedArr = new int[ACTUALisvBoxed.length];
 
 		for (int i = 0; i != unboxedArr.length; ++i)
 		{
-			unboxedArr[i] = swizzleVecBoxed[i];
+			unboxedArr[i] = ACTUALisvBoxed[i];
 		}
 
 		return unboxedArr;
@@ -365,10 +365,10 @@ public final class TxtShuffle {
 
 
 	/*
-	 * To encode, we want to convert a big number (or whatever) into a 'swizzle vector' (a 'pivot vector'?),
-	 * then apply that swizzle vector to the source data. (Assume element-uniqueness for now.)
+	 * To encode, we want to convert a big number (or whatever) into an ACTUALISV (a 'pivot vector'?),
+	 * then apply that ACTUALISV to the source data. (Assume element-uniqueness for now.)
 	 *
-	 * To decode, we want to figure out what swizzle vector was applied, then map that back
+	 * To decode, we want to figure out what ACTUALISV was applied, then map that back
 	 * to a big number (or whatever).
 	 */
 
@@ -387,12 +387,14 @@ public final class TxtShuffle {
 	 * which we can then reorder in a unique and reversible way.
 	 *
 	 *
-	 * Definition of a swizzle vector:
+	 * Definition of an ACTUALISV:
+   *
+   * TODO rework this to emphasise the 'inverse' part of ISV
 	 *
-	 * A swizzle vector is a data structure which maps each entity to its index in the output array.
-	 * Applying an swizzle vector, then, reorders the input array in a unique way (giving a unique output).
+	 * An ACTUALISV is a data structure which maps each entity to its index in the output array.
+	 * Applying an ACTUALISV, then, reorders the input array in a unique way (giving a unique output).
 	 *
-	 * It's name reflects the 'swizzle' feature of, for example, OpenCL C, where
+	 * Its name reflects the 'swizzle' feature of, for example, OpenCL C, where
 	 *   myVec1.xyz = myVec1.zyx;
 	 * will reverse the elements of the myVec1 vector.
 	 *
@@ -402,19 +404,19 @@ public final class TxtShuffle {
 	 * means we should do
 	 *   outputArray[m] = inputArray[n]
 	 *
-	 * REPHRASE THIS We can represent a swizzle vector as a number,
-	 * such that each swizzle vector corresponds to
+	 * REPHRASE THIS We can represent an ACTUALISV as a number,
+	 * such that each ACTUALISV corresponds to
 	 * exactly one number in an interval from 0 to some max. number,
-	 * and such that each number in that interval corresponds to exactly one swizzle vector.
+	 * and such that each number in that interval corresponds to exactly one ACTUALISV.
 	 *
 	 * To do this mapping, it's easier to first consider the function which takes us from
-	 * the swizzle vector to the unique number.
+	 * the ACTUALISV to the unique number.
 	 *
 	 * REWRITE THIS:
 	 *
-	 * Given a swizzle vector of length L:
+	 * Given an ACUTALISV of length L:
 	 * set up an accumulator, initializing at 0
-	 * iterate through the swizzle vector, and for each element:
+	 * iterate through the ACTUALISV, and for each element:
 	 *   'multiply up' the accumulator to 'make space' for this slot.
 	 *   For the first element that means do nothing, for subsequent ones,
 	 *   multiply by MAXPOSSIBLE-1....
@@ -437,17 +439,17 @@ public final class TxtShuffle {
 	 * iterating through
 	 * the input array and
 	 *
-	 * Decoding a swizzle vector:
+	 * Decoding an ACTUALISV:
 	 * Given a map of n many elements,
 	 *
 	 *
 	 *
-	 * Generating the swizzle vector:
+	 * Generating the ACTUALISV:
 	 *
 	 *
 	 * So, using the intrinsic approach:
 	 * 1.   Sort the annotated array.
-	 * 2.   Consulting the swizzle vector and the now-sorted annotated array, write the output data
+	 * 2.   Consulting the ACTUALISV and the now-sorted annotated array, write the output data
 	 */
 
 //	public static void main(String[] args) {
